883483bed04b6dd0721218674f761bfea9815ff3
jalew188 <jalew.zwf@qq.com>
2020-11-23 21:20:52 +0100

    GlyGly[K] modloss priority

--- pDeep/psmLabel/modloss_priority.py
+++ pDeep/psmLabel/modloss_priority.py
@@ -2,5 +2,6 @@ priority = {}
 priority["Phospho[S]"] = 1e8
 #pS>pT??, pS is more easy to generate modloss ions
 priority["Phospho[T]"] = 1e7
+priority["GlyGly[K]"] = 1e6
 #pS/pT > oM??
 priority["Oxidation[M]"] = 1e5
